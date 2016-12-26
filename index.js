43b976a8777a1c1b2a9ae7ef96d2ecb1a149afff
cfrancobedoya <cfrancobedoya@gmail.com>
2016-12-26 16:12:49 -0500

    Se crea una página estática para el acceso de los empleados de MK Capital SAS

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+const config = require('./config')
+
+
+app.listen(config.port , function (){
+    console.log(`Aplicación corriendo en htpp://localhost:${config.port}`)
+    console.log("Google MK Capital S.A.S");
+    console.log("Desarrollado por: Cristian Franco Bedoya");
+    console.log("Franco Software 2016");
+});
